b48ccb91dd3c2c96a243e95019fe4d009af956a3
Luciano Oliveira <luciano.oliveira96@yahoo.com.br>
2024-04-17 23:07:05 -0300

    update contador

--- script.js
+++ script.js
@@ -1,91 +1,94 @@
+eventos = document.querySelector("#total");
+cards = document.querySelectorAll(".card");
+evtType = document.querySelector("#evt-select");
 
-document.addEventListener("DOMContentLoaded", () => {
+    let total = cards.length;
+    eventos.innerHTML = total;
 
-    eventos = document.querySelector("#total");
-    cards = document.querySelectorAll(".card");
-    evtType = document.querySelector("#evt-select");
-    
-    // let total = cards.length;
-    // eventos.innerHTML = total;
+evtType.addEventListener("change", () => {
 
-    evtType.addEventListener("change", () => {
+    total = 0;
 
-        total = 0;
-        if (evtType.value == "Todos") {
-            cards.forEach(element => {
+    if (evtType.value == "Todos") {
+        cards.forEach(element => {eventos
+            element.style.display = "block";
+            total ++;
+        })
+        eventos.innerHTML = total;
+    }
+
+    if (evtType.value == "Apresentação") {
+        
+        cards.forEach(element => {
+            if (element.classList.contains("apresentacao")) {
                 element.style.display = "block";
                 total ++;
-            })
-            console.log("Total:" + total);
-            eventos.innerHTML = total;
-        }
-        if (evtType.value == "Apresentação") {
-            eventos.innerHTML = '';
-            cards.forEach(element => {
-                if (element.classList.contains("apresentacao")) {
-                    element.style.display = "block";
-                    total ++;
-                    
-                } else {
-                    element.style.display = "none";
-                }
-            })
-            console.log("Total:" + total);
-            eventos.textContent  = total;
-        }
-        else if (evtType.value == "Exposição") {
-            cards.forEach(element => {
-                if (element.classList.contains("exposicao")) {
-                    element.style.display = "block";
-                } else {
-                    element.style.display = "none";
-                }
-            })
-        }
-        if (evtType.value == "Passeios") {
-            cards.forEach(element => {
-                if (element.classList.contains("passeios")) {
-                    element.style.display = "block";
-                } else {
-                    element.style.display = "none";
-                }
-            })}
-        if (evtType.value == "Oficinas") {
-            cards.forEach(element => {
-                if (element.classList.contains("Oficinas")) {
-                    element.style.display = "block";
-                    total ++;
-                } else {
-                    element.style.display = "none";
-                }
-            })
-            alert("Total:" + total);
-            console.log("Total:" + total);
-            eventos.innerHTML = total;
-        }
-        if (evtType.value == "Artes") {
-            cards.forEach(element => {
-                if (element.classList.contains("artes")) {
-                    element.style.display = "block";
-                } else {
-                    element.style.display = "none";
-                }
-            })
-        }
-        if (evtType.value == "Musica") {
-            cards.forEach(element => {
-                if (element.classList.contains("musica")) {
-                    element.style.display = "block";
-                } else {
-                    element.style.display = "none";
-                }
-            })
-        }
-        total = cards.length;
+            } else {
+                element.style.display = "none";
+            }
+        })
         eventos.innerHTML = total;
-    })
-    
+    }
+
+    if (evtType.value == "Exposição") {
+        cards.forEach(element => {
+            if (element.classList.contains("exposicao")) {
+                element.style.display = "block";
+                total ++;
+            } else {
+                element.style.display = "none";
+            }
+        })
+        eventos.innerHTML = total;
+    }
+    if (evtType.value == "Passeios") {
+        cards.forEach(element => {
+            if (element.classList.contains("passeios")) {
+                element.style.display = "block";
+                total ++;
+            } else {
+                element.style.display = "none";
+            }
+        })
+        eventos.innerHTML = total;
+    }
+
+    if (evtType.value == "Oficinas") {
+        cards.forEach(element => {
+            if (element.classList.contains("Oficinas")) {
+                element.style.display = "block";
+                total ++;
+            } else {
+                element.style.display = "none";
+            }
+        })
+        eventos.innerHTML = total;
+    }
+    if (evtType.value == "Artes") {
+        cards.forEach(element => {
+            if (element.classList.contains("artes")) {
+                element.style.display = "block";
+                total ++;
+            } else {
+                element.style.display = "none";
+            }
+        })
+        eventos.innerHTML = total;
+    }
+    if (evtType.value == "Musica") {
+        cards.forEach(element => {
+            if (element.classList.contains("musica")) {
+                element.style.display = "block";
+                total ++;
+            } else {
+                element.style.display = "none";
+            }
+        })
+        eventos.innerHTML = total;
+    }
 })
+    
+
 
 
 
